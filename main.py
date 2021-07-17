3222e344c6f0811f1ca6c89b62e52988c4a3cc10
stefanogrippa <35608852+stefanogrippa@users.noreply.github.com>
2021-07-17 19:15:05 +0200

    heston

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # https://www.codearmo.com/python-tutorial/merton-jump-diffusion-model-python
 
+# modello di Merton con jumps a volatilita costante
+
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import norm
@@ -32,6 +34,8 @@ Lam = 1  # intensity of jump i.e. number of jumps per year
 Steps = 10000  # time steps
 Npaths = 2  # number of paths to simulate
 Sigma = 0.2  # annual standard deviation , for Weiner process
+
+K = 100  # strike price (l'opzione in questo momento e' at the money)
 
 j = merton_jump_paths(S, T, R, Sigma, Lam, M, V, Steps, Npaths)
 
@@ -98,7 +102,7 @@ def implied_vol(opt_value, s, k, t, r, type_='call'):
         raise ValueError("type_ must be 'put' or 'call'")
 
 
-K = 100
+
 np.random.seed(3)
 j = merton_jump_paths(S, T, R, Sigma, Lam, M, V, Steps, Npaths)  # generate jump diffusion paths
 
@@ -173,3 +177,125 @@ plt.legend()
 plt.xlabel('Strike')
 plt.ylabel('Value in $')
 plt.title('Merton Model Optimal Params')
+plt.show()
+
+# https://www.codearmo.com/python-tutorial/heston-model-simulation-python
+rho = -0.7
+Ndraws = 1000
+mu = np.array([0, 0])
+cov = np.array([[1, rho], [rho , 1]])
+
+W = np.random.multivariate_normal(mu, cov, size=Ndraws)
+
+plt.plot(W.cumsum(axis=0))
+plt.title('Correlated Random Variables')
+plt.show()
+print(np.corrcoef(W.T))
+
+
+def generate_heston_paths(S, T, r, kappa, theta, v_0, rho, xi,
+                          steps, Npaths, return_vol=False):
+    dt = T / steps
+    size = (Npaths, steps)
+    prices = np.zeros(size)
+    sigs = np.zeros(size)
+    S_t = S
+    v_t = v_0
+    for t in range(steps):
+        WT = np.random.multivariate_normal(np.array([0, 0]),
+                                           cov=np.array([[1, rho],
+                                                         [rho, 1]]),
+                                           size=paths) * np.sqrt(dt)
+
+        S_t = S_t * (np.exp((r - 0.5 * v_t) * dt + np.sqrt(v_t) * WT[:, 0]))
+        v_t = np.abs(v_t + kappa * (theta - v_t) * dt + xi * np.sqrt(v_t) * WT[:, 1])
+        prices[:, t] = S_t
+        sigs[:, t] = v_t
+
+    if return_vol:
+        return prices, sigs
+
+    return prices
+
+kappa =4
+theta = 0.02
+v_0 =  0.02
+xi = 0.9
+r = 0.02
+S = 100
+paths =50000
+steps = 2000
+T = 1
+
+prices_pos = generate_heston_paths(S, T, r, kappa, theta,
+                                    v_0, rho=0.9, xi=xi, steps=steps, Npaths=paths,
+                                    return_vol=False)[:,-1]
+prices_neg  = generate_heston_paths(S, T, r, kappa, theta,
+                                    v_0, rho=-0.9, xi=xi, steps=steps, Npaths=paths,
+                                    return_vol=False)[:,-1]
+gbm_bench = S*np.exp( np.random.normal((r - v_0/2)*T ,
+                                np.sqrt(theta)*np.sqrt(T), size=paths))
+
+import seaborn as sns
+
+fig, ax = plt.subplots()
+
+ax = sns.kdeplot(data=prices_pos, label=r"$\rho = 0.9$", ax=ax)
+ax = sns.kdeplot(data=prices_neg, label=r"$\rho= -0.9$ ", ax=ax)
+ax = sns.kdeplot(data=gbm_bench, label="GBM", ax=ax)
+
+ax.set_title(r'Tail Density by Varying $\rho$')
+plt.axis([40, 180, 0, 0.055])
+plt.xlabel('$S_T$')
+plt.ylabel('Density')
+plt.show()
+
+strikes =np.arange(30, 200,1)
+
+puts = []
+
+for K in strikes:
+    P = np.mean(np.maximum(K-prices_neg,0))*np.exp(-r*T)
+    puts.append(P)
+
+
+ivs = [implied_vol(P, S, K, T, r, type_ = 'put' ) for P, K in zip(puts,strikes)]
+
+plt.plot(strikes, ivs)
+plt.ylabel('Implied Volatility')
+plt.xlabel('Strike')
+plt.axvline(S, color='black',linestyle='--',
+            label='Spot Price')
+plt.title('Implied Volatility Smile from Heston Model')
+plt.legend()
+plt.show()
+
+kappa = 3
+theta = 0.04
+v_0 = 0.04
+xi = 0.6
+r = 0.05
+S = 100
+paths = 3
+steps = 10000
+T = 1
+rho = -0.8
+prices, sigs = generate_heston_paths(S, T, r, kappa, theta,
+                                     v_0, rho, xi, steps, paths,
+                                     return_vol=True)
+
+plt.figure(figsize=(7, 6))
+plt.plot(prices.T)
+plt.title('Heston Price Paths Simulation')
+plt.xlabel('Time Steps')
+plt.ylabel('Stock Price')
+plt.show()
+
+plt.figure(figsize=(7, 6))
+plt.plot(np.sqrt(sigs).T)
+plt.axhline(np.sqrt(theta), color='black', label=r'$\sqrt{\theta}$')
+plt.title('Heston Stochastic Vol Simulation')
+plt.xlabel('Time Steps')
+plt.ylabel('Volatility')
+plt.legend(fontsize=15)
+plt.show()
