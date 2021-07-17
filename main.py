c548a269ed75b2647ebadb1cd28de8f1d43300b0
stefanogrippa <35608852+stefanogrippa@users.noreply.github.com>
2021-07-17 11:50:19 +0200

    levati warning

--- main.py
+++ main.py
@@ -6,29 +6,29 @@ import numpy as np
 plt.style.use('ggplot')
 
 
-def merton_jump_paths(S, T, r, sigma, lam, m, v, steps, Npaths):
-    size = (steps, Npaths)
-    dt = T / steps
+def merton_jump_paths(s, t, r, sigma, lam, m, v, steps, nofpaths):
+    size = (steps, nofpaths)
+    dt = t / steps
     poi_rv = np.multiply(np.random.poisson(lam * dt, size=size),
                          np.random.normal(m, v, size=size)).cumsum(axis=0)
     geo = np.cumsum(((r - sigma ** 2 / 2 - lam * (m + v ** 2 * 0.5)) * dt +
                      sigma * np.sqrt(dt) *
                      np.random.normal(size=size)), axis=0)
 
-    return np.exp(geo + poi_rv) * S
+    return np.exp(geo + poi_rv) * s
 
 
-S = 100  # current stock price
+S = 80  # 100  # current stock price
 T = 1  # time to maturity
-r = 0.02  # risk free rate
-m = 0  # meean of jump size
-v = 0.3  # standard deviation of jump
-lam = 1  # intensity of jump i.e. number of jumps per annum
-steps = 10000  # time steps
+R = 0.03  # 0.02  # risk free rate
+M = 0.02  # 0  # mean of jump size
+V = 0.2  # 0.3  # standard deviation of jump
+Lam = 0.1  # 1  # intensity of jump i.e. number of jumps per year
+Steps = 10000  # time steps
 Npaths = 10  # number of paths to simulate
-sigma = 0.2  # annaul standard deviation , for weiner process
+Sigma = 0.2  # annual standard deviation , for Weiner process
 
-j = merton_jump_paths(S, T, r, sigma, lam, m, v, steps, Npaths)
+j = merton_jump_paths(S, T, R, Sigma, Lam, M, V, Steps, Npaths)
 
 plt.plot(j)
 plt.xlabel('Days')
